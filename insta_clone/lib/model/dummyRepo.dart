803f3b0f5b82df7a70bbe686f8af06508ca19de2
Yxxnsu <dustn0121@naver.com>
2021-07-22 21:20:02 +0900

    [ADD] Dummy model

---
+++ insta_clone/lib/model/dummyRepo.dart
@@ -0,0 +1,77 @@
+List<Map<String, dynamic>> dummyList = [
+  {
+    "name": "Cristiano IMYEONSU",
+    "userId": "YXXNSU",
+    "live": false,
+    "vista": false,
+    "profileURL":"assets/me.png",
+    "dashes": 1,
+  },
+  {
+    "name": "Leonel Messi",
+    "userId": "Leonel Yxxnsu",
+    "live": true,
+    "vista": false,
+    "profileURL":"https://pbs.twimg.com/media/B338156CIAAABM_.jpg",
+    "dashes": 14,
+    "url_image_post":"https://cdn.futbolargentino.com/sdi/2018/09/09/lionel-messi-y-su-mensaje-a-la-seleccion-argentina-via-instagram-669809.jpg",
+  },
+  {
+    "name": "Kim Chan Hee",
+    "userId": "Yxxnsu_0",
+    "live": true,
+    "vista": false,
+    "profileURL":"https://juanbustos.com/wp-content/uploads/2018/10/Selfie.jpg",
+    "dashes": 2,
+    "url_image_post":"https://mapio.net/images-p/1172789.jpg",
+  },
+  {
+    "name": "Jo In Sung",
+    "userId": "Yxxsnu_1",
+    "live": false,
+    "vista": false,
+    "profileURL": "https://i.ytimg.com/vi/gLxO5R61hFI/maxresdefault.jpg",
+    "dashes": 5,
+    "url_image_post":"https://d35hsl9am8s2ta.cloudfront.net/public/images/2020/09/15991530940-La-Boca-773x458.jpg",
+  },
+  {
+    "name": "Jung Bo Mun",
+    "userId": "Yxxnsu_2",
+    "live" : false,
+    "vista": false,
+    "profileURL":
+    "https://i.pinimg.com/564x/3b/e1/e5/3be1e5f82073d219df18ea2b09db1f3a.jpg",
+    "dashes": 2,
+    "url_image_post":"https://intainforma.inta.gob.ar/wp-content/uploads/2020/04/Agricultura_urbana-web.jpg",
+  },
+  {
+    "name": "Jo Seung Jin",
+    "userId": "Yxxnsu_2",
+    "live": false,
+    "vista": false,
+    "profileURL":
+    "https://i.pinimg.com/474x/77/56/06/775606b331fcb0a44dad916143bddf61.jpg",
+    "dashes": 1,
+    "url_image_post":"https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/06/09/15600711667471.png",
+  },
+  {
+    "name": "Manuel Albastro",
+    "userId": "Yxxnsu_2",
+    "live": false,
+    "vista": false,
+    "profileURL":
+    "https://www.okchicas.com/wp-content/uploads/2015/10/chico-transg%C3%A9nero-toma-una-selfie-cada-d%C3%ADa-4.jpg",
+    "dashes":0,
+    "url_image_post":"https://i.blogs.es/79fe25/traajos-en-programacion/1366_2000.jpg",
+  },
+  {
+    "name": "Hong Seung Hun",
+    "userId": "Yxxnsu_2",
+    "live": false,
+    "vista": false,
+    "profileURL":
+    "https://viapais.cdncimeco.com/media/cache/resolve/medium/http://viapais.com.ar/files/2017/10/20171020160548_30251308_0_body.jpg",
+    "dashes":0,
+    "url_image_post":"https://www.dzoom.org.es/wp-content/uploads/2007/02/canon-mejorar-fotografo-consejos-principiantes-novatos-810x540.jpg",
+  },
+];
