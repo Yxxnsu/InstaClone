17ebb79111eb939f77e334d8f88d22a6f7276456
Yxxnsu <dustn0121@naver.com>
2021-07-23 00:41:38 +0900

    [UPDATE] Login.dart , validate

--- insta_clone/lib/main.dart
+++ insta_clone/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-import 'login.dart';
+import 'view/login.dart';
 void main() {
   runApp(MyApp());
 }
